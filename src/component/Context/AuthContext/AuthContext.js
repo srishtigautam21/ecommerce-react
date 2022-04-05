b1cbed3bf88fef67b836a7266709df723f3fefe4
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-04-05 14:39:49 +0530

    Removed commented code

--- src/component/Context/AuthContext/AuthContext.js
+++ src/component/Context/AuthContext/AuthContext.js
@@ -19,17 +19,15 @@ const AuthProvider = ({ children }) => {
   });
   const [signupUser, setSignUpUser] = useState(authInitialState);
   const [isUserLoggedIn, setUserLoggedIn] = useState(false);
-  // const { email, password } = loginUser;
 
   const loginHandler = async (e, email, password) => {
     e.preventDefault();
-    // console.log("login");
 
     try {
       const response = await axios.post("/api/auth/login", { email, password });
-      // loginUser
+
       localStorage.setItem("nurishToken", response.data.encodedToken);
-      // console.log("auth", response.data.encodedToken);
+
       setUserLoggedIn(true);
       // setLoginUser({ email: "", password: "" });    //Kept this for future use
       loginToast("Login Successful");
